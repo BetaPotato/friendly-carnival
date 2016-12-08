6c0849537fd3a3b9cd05faab0fc55e62bbf6db27
Jaebaek Lee <>
2016-12-08 13:30:39 -0600

    Merge origin/master
    Conflicts:
    	src/Vulnerabilities.java

--- src/vulMap.java
+++ src/vulMap.java
@@ -17,34 +17,18 @@ public class vulMap {
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
- 
+    
 /**
  *
  * @author lee_866317
  */
-    public TreeMap<Integer, team> teams;
-    public ArrayList<league> leagues;
+    public TreeMap<String, scanCommands> scans;
     
-    public model(){
-        teams = new TreeMap();
-        leagues = new ArrayList();
+    public vulMap(){
+        scans = new TreeMap();
     }
     
-    public TreeMap<Integer, team> getTeams() {
-        return teams;
+    public TreeMap<String, scanCommands> getScans() {
+        return scans;
     }
-
-    public void setTeams(TreeMap<Integer, team> teams) {
-        this.teams = teams;
-    }
-
-    public ArrayList<league> getLeagues() {
-        return leagues;
-    }
-
-    public void setLeagues(ArrayList<league> leagues) {
-        this.leagues = leagues;
-    }
-}
-
 }
