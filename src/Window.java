444b13b8ba381b16006ebfe1f38e63a2a892b737
Ryan-Galligher <ryanpg99@gmail.com>
2016-12-30 18:37:11 -0600

    Merge origin/master into temp2
    Conflicts:
    	nbproject/private/private.xml

--- src/Window.java
+++ src/Window.java
@@ -19,16 +19,34 @@ public interface Window
 
     static final ExecutorService exec = Executors.newSingleThreadExecutor();
     
+    /**
+     * 
+     * @param vulns This is an ArrayList of Vulnerabilities that the ScoringBot will be looking for on the computer to see if they have been solved or not.
+     * @param whichOS a boolean of which Operating System the Vulnerability is allowed to <b>run on</b>, NOT that the computer is running on. Should only be {@link #WINDOWS} or {@link #LINUX}.
+     * @return A {@code Future<?>} that is given back from the {@code ExecutorService} which is the only real way to access the {@link ScoringBot} Thread.
+     */
     default Future<?> startScoringBot(ArrayList<Vulnerability> vulns, boolean whichOS)
     {
         return exec.submit(new ScoringBot(vulns, whichOS, this));
     }
     
+    /**
+     * This is the method that must be called in order to stop the ScoringBot properly. 
+     * @param fut This is the {@code Future<?>} that must be given back from {@link StartScoringBot(ArrayList<Vulnerability> vulns, boolean whichOS)}.
+     */
     default void stopScoringBot(Future<?> fut)
     {
         fut.cancel(true);
     }
     
+    /**
+     * This method needs to be implemented to allow the {@link ScoringBot} to call the {@code Window} and update the GUI with the proper score.
+     * @param vuln A {@link Vulnerability} that has been solved and needs to be updated in the GUI.
+     */
     void vulnSolved(Vulnerability vuln);
+    /**
+     * This method needs to be implemented to allow the {@link ScoringBot} to call the {@code Window} and update the GUI with the proper score.
+     * @param vuln A {@link Vulnerability} that has been unsolved and needs to be updated in the GUI.
+     */
     void vulnUnSolved(Vulnerability vuln);
 }
